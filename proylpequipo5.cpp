e4cca6a07fe192015ecfb1434871d50f3a43d414
jonny-jacome <jonny_jacome@hotmailcom>
2015-07-10 23:13:52 -0500

    Avance
    Creacion primer menu principal

--- proylpequipo5.cpp
+++ proylpequipo5.cpp
@@ -1,1 +1,29 @@
-#include<conio>
+#include <conio>
+#include <iostream>
+
+main()
+{
+int opcion;
+cout<<"+++++++---------ESCUELA SUPERIOR POLITECNICA DE CHIMBORAZO-------+++++++++++++++\n";
+cout<<"+++++++++++++++-------LENGUAJES DE PROGRAMACION----------+++++++++++++++++++++++\n";
+cout<<"Titulo del Proyecto: Horario de clases\n";
+cout<<"MENU PRINCIPAL\n";
+cout<<"1.Ingresar una nueva hora de clases\n";
+cout<<"2.Ver el horario de hoy\n";
+cout<<"3.Ver el horario de la semana\n";
+cout<<"4.Salir\n";
+cout<<"SELECCIONE UNA OPCION\n";
+cin>>opcion;
+switch(opcion)
+{
+case 1:
+break;
+case 2:
+break;
+case 3:
+break;
+case 4:
+break;
+}
+getch();
+}
